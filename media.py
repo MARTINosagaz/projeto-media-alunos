e4779719a240c4417c831e1516ce7edb168c2065
MARTINosagaz <martinkauan434@gmail.com>
2024-11-20 11:51:46 -0300

    add refatorando a media

--- media.py
+++ media.py
@@ -2,7 +2,7 @@ nota1 = int(input("digite a primeira nota "))
 nota2 = int(input("digite a segunda nota "))
 nota3 = int(input("digite a terceira nota "))
 nota4 = int(input("digite a quarta nota "))
-media = (nota1 + nota2 + nota3 + nota4)/4
+media = (nota1*1 + nota2*1 + nota3*2 )/4
 print(media)
 if media > 6 :
  print ("aprovado")
