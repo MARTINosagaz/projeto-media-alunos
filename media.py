898bf45493e055c5714383c51d633bc5800d2fdd
MARTINosagaz <martinkauan434@gmail.com>
2024-11-20 11:39:52 -0300

    add media em py

---
+++ media.py
@@ -0,0 +1,6 @@
+nota1 = int(input("digite a primeira nota "))
+nota2 = int(input("digite a segunda nota "))
+nota3 = int(input("digite a terceira nota "))
+nota4 = int(input("digite a quarta nota "))
+media = (nota1 + nota2 + nota3 + nota4)/4
+print(media)
